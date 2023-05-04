26fce625c867e5f8c3ccc41f56eec5d607b05547
jemishaRakholiya <132455415+jemishaRakholiya@users.noreply.github.com>
2023-05-04 15:21:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,24 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded());
 app.use('/upload',express.static(path.join(__dirname+'/upload')));
 
-const db = require('./confing/mongoose');
+
+const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportlocal = require('./confing/passport-local-strategy');
 const pasportuser = require('./confing/passport-user-strategy');
 
 app.use(express.static('assets'));
-        
+
+mongoose.connect('mongodb+srv://rakholiyajemisha13:1342003jemi@cluster0.rcmgfvi.mongodb.net/test',{
+     useNewUrlParser : true, 
+     useUnifiedTopology : true 
+  }).then(()=>{ 
+      console.log('DB Connect'); 
+  }).catch(err =>{ 
+      console.log('DB Not Connected !!',err); 
+})
+
 app.use(session({
     name : 'jemisha',
     secret : 'me',
